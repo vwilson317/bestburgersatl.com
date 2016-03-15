5a0dd2b2a46a496274499cceaf7a41c0c88a2155
Vincent Wilson <vincent.j.wilson@gmail.com>
2016-03-15 11:46:25 -0400

    Added dapper code to connect to rds db to get post data

--- BestBurgersAtl/src/BestBurgersAtl/Resources/DA/PostRepository.cs
+++ BestBurgersAtl/src/BestBurgersAtl/Resources/DA/PostRepository.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace BestBurgersAtl.Resources.DA
 {
     public class PostRepository : IPostRepository// LookupRepositoryBase<PostResource>, IPostRepository
     {
+        SqlConnection Connection { get; set; }
+        private string CONNECTION_STRING = "Server=bbatlsqlexpress.cibtf39dd2gv.us-east-1.rds.amazonaws.com,1433\\SQLEXPRESS;Database=BestBurgersAtl;User ID=admin;Password=falcons1;";
         public  IEnumerable<PostResource> GetAll()
         {
-            return Enumerable.Repeat(new PostResource { Description = "Test Post", ContentText = "Mock post no html" }, 1);
+            using (Connection = new SqlConnection(CONNECTION_STRING))
+            {
+                return Connection.Query<PostResource>("select * from dbo.Post");
+            }
         }
     }
 }
--- BestBurgersAtl/src/BestBurgersAtl/Resources/PostResource.cs
+++ BestBurgersAtl/src/BestBurgersAtl/Resources/PostResource.cs
@@ -7,37 +7,14 @@ namespace BestBurgersAtl.Resources
 {
     public class PostResource : IPostResource
     {
-        public string ContentText
-        {
-            get;
-
-            set;
-        }
-
+        public string ContentText { get; set; }
         public DateTime CreateDt { get; set; }
-
-        public string Description
-        { get; set; }
-
-        public string DisplayImageUrl
-        {
-            get; set;
-        }
-
-        public int Id
-        {
-            get; set;
-        }
-
-        public string SEO
-        {
-            get; set;
-        }
-
-        public DateTime? UpdateDt
-        {
-            get; set;
-        }
+        public string Description { get; set; }
+        public string DisplayImageUrl { get; set; }
+        public int Id { get; set; }
+        public string SEO { get; set; }
+        public DateTime? UpdateDt { get; set; }
+        public int Views { get; set; }
     }
 
     public interface IPostResource
